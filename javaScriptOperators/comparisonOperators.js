5bf49c12e2d58a2c0a4de9914bc2c724883c48a7
Kyle Martin <martkyle@gmail.com>
2019-02-26 21:52:16 -0500

    JavaScript Comparison Operators
    Starting to learn comparison operators

---
+++ javaScriptOperators/comparisonOperators.js
@@ -0,0 +1,13 @@
+//If age is negative - print an error message
+if(age < 0) {
+	console.log("Error - age undefined");
+}
+
+//If age is 21 - print "Happy 21st birthday!!"
+//If age is odd - print "Your age is odd!"
+else if(age < 21) {
+	console.log("Happy 21st birthday!!");
+}
+
+//BONUS - If age is perfect square - print "Perfect Square!!!"
+
