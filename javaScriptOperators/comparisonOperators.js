f3e50cec9162c100347a8ab890047e0344ef7abb
Kyle Martin <martkyle@gmail.com>
2019-02-27 21:35:55 -0500

    JavaScript Comparison Operators
    Adding operators to compare the age

--- javaScriptOperators/comparisonOperators.js
+++ javaScriptOperators/comparisonOperators.js
@@ -1,13 +1,20 @@
+// Get age and convert it to a Number (prompt always returns a String)
+var age = Number(prompt("What is your age?"));
+
 //If age is negative - print an error message
 if(age < 0) {
-	console.log("Error - age undefined");
+	console.log("Error - Is your age for real??");
 }
 
 //If age is 21 - print "Happy 21st birthday!!"
+if(age === 21) {
+	console.log("Happy 21st birthday!!")
+}
+
 //If age is odd - print "Your age is odd!"
-else if(age < 21) {
+if(age < 21) {
 	console.log("Happy 21st birthday!!");
 }
 
 //BONUS - If age is perfect square - print "Perfect Square!!!"
-
+if(age )
