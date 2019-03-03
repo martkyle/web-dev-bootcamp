bdafd5dcded0f4c9b771d45101276394dffbb357
Kyle Martin <martkyle@gmail.com>
2019-03-03 13:47:54 -0500

    JavaScript While Loops
    Learning how to code 'While' loops

---
+++ javaScriptOperators/whileLoop.js
@@ -0,0 +1,58 @@
+ /*
+ -- Count 1-5
+ var count = 1;
+ while(count < 6) {
+  console.log("count is: " + count);
+  count++;
+ }
+
+ -- Print "Hello"
+ string we're looping over:
+  var str = "hello";
+ first character is at index 0
+  var count = 0;    
+  while(count < str.length) {
+   console.log(str[count]);
+   count++;
+  }
+
+ -- Counts odd numbers
+ var num = 1;
+ while(num <= 10) {
+   console.log(num);
+   num += 2;
+ }
+
+ -- Finds the numbers divided by 4 into 20
+ var num = 1;
+ while(num <= 20) {
+  if(num % 4 === 0){
+    console.log(num);
+  }
+  num++;
+ }
+*/
+
+
+
+
+
+// Print all numbers between -10 and 19 -- PRINT ALL NUMBERS BETWEEN -10 AND 19
+var num = 1;
+
+while(num <= 19) {
+	console.log("PRINT ALL NUMBERS BETWEEN -10 AND 19");
+	if(-10 num++) {
+	console.log(num)
+}
+}
+
+// Print all even numbers between 10 and 40
+
+
+// Print all odd numbers between 300 and 333
+
+
+// Print all numbers divisible by 5 AND 3 between 5 and 50
+
+
